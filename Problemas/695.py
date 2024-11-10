09a8058bcd8b2cbf538a8daf391767be964bc5c4
vinialves2020 <vinicius.livramentofga@gmail.com>
2024-11-10 19:42:14 -0300

    Mudança do codigo

--- Problemas/695.py
+++ Problemas/695.py
@@ -1,15 +1,35 @@
 class Solution:
     def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
-        Linhas, Colunas = len(grid) , len(grid[0])
-        visit = set()
+        def eh_valido(linha, coluna):
+            return (0 <= linha < len(grid) and 
+                    0 <= coluna < len(grid[0]) and 
+                    grid[linha][coluna] == 1 and 
+                    (linha, coluna) not in visitados)
         
-        def dfs(l,c):
-            if(l < 0 or l == Linhas or c<0 or c == Colunas or grid[l][c] == 0 or (l,c) in visit):
-                return 0
-            visit.add((l,c))
-            return (1 + dfs(l + 1 ,c) + dfs(l - 1 ,c) + dfs(l ,c +1 ) +dfs(l ,c - 1))
-        area = 0
-        for l in range(Linhas):
-            for c in range(Colunas):
-                area= max(area,dfs(l,c))
-        return area 
+        def dfs_iterativo(linha, coluna):
+            pilha = [(linha, coluna)]
+            area = 0
+            
+            while pilha:
+                x, y = pilha.pop()
+                if (x, y) in visitados:
+                    continue
+                
+                visitados.add((x, y))
+                area += 1
+                for dx, dy in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
+                    nova_linha, nova_coluna = x + dx, y + dy
+                    if eh_valido(nova_linha, nova_coluna):
+                        pilha.append((nova_linha, nova_coluna))
+            
+            return area
+        
+        visitados = set()
+        maior_area = 0
+        
+        for linha in range(len(grid)):
+            for coluna in range(len(grid[0])):
+                if grid[linha][coluna] == 1 and (linha, coluna) not in visitados:
+                    maior_area = max(maior_area, dfs_iterativo(linha, coluna))
+        
+        return maior_area
